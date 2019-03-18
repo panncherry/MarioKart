30041522da40d613c9115f7a7ea6b5121fe52fc9
panncherry <maung.eainmatmay@gmail.com>
2019-03-17 23:15:42 -0400

    add rotate gesture func

--- MarioKart/ViewController.swift
+++ MarioKart/ViewController.swift
@@ -12,12 +12,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
     @IBAction func didPanKartView(_ sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: view)
+        print("Location: x: \(location.x), y: \(location.y)")
+        let kartView = sender.view!
+        kartView.center = location
     }
     
+    @IBAction func didPinchKartView(_ sender: UIPinchGestureRecognizer) {
+        let scale = sender.scale
+        print("scale: \(scale)")
+        let kartView = sender.view!
+        kartView.transform = CGAffineTransform(scaleX: scale, y: scale)
+    }
+    
+    
+    @IBAction func didRotateKartView(_ sender: UIRotationGestureRecognizer) {
+    }
 }
 
